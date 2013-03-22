7bdc9c59648abb5c50e1415ce9bbdee90361e33c
Moritz <moritz@tibidat.com>
2013-03-22 10:08:37 +0900

    Initial, barebones commit.

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "static/css"
+sass_dir = "_sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+
+output_style = :compressed
+
+# relative_assets = true
+
+line_comments = false
