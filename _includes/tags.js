f849479f135e6d2ce75705a3555dc8d089ae9c21
Moritz <moritz@tibidat.com>
2014-10-02 14:29:53 +0200

    Fixed whitespace in tags.js

--- _includes/tags.js
+++ _includes/tags.js
@@ -1,10 +1,10 @@
 $('a.tag-name').click(function(e){
-	var tag_name;
-	$('.hidden').hide(); // hide any visible data
-	tag_name = e.target.id.split('-')[0];
-	$('#tag-name-placeholder').text(tag_name); // populate the details header
-	$('#tag-data-header').show();
-	$('#' + tag_name + '-posts').show(); // show recent posts
-	e.preventDefault();
-	return false;
+    var tag_name;
+    $('.hidden').hide(); // hide any visible data
+    tag_name = e.target.id.split('-')[0];
+    $('#tag-name-placeholder').text(tag_name); // populate the details header
+    $('#tag-data-header').show();
+    $('#' + tag_name + '-posts').show(); // show recent posts
+    e.preventDefault();
+    return false;
 });
